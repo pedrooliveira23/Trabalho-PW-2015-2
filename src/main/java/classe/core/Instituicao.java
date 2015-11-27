75be4dfab27c9ddb474126741cd9aa4c76e46477
pedrooliveira23 <pedrooliveira2309@gmail.com>
2015-11-27 21:38:53 -0200

    Classe da Instituição

---
+++ src/main/java/classe/core/Instituicao.java
@@ -0,0 +1,62 @@
+package classe.core;
+
+public class Instituicao {
+	private String nome, nivelCurso, endereco, telefone, email,
+			nomeResponsavel, telefoneResponsavel, emailResponsavel;
+	private Cnpj cnpj;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNivelCurso() {
+		return nivelCurso;
+	}
+	public void setNivelCurso(String nivelCurso) {
+		this.nivelCurso = nivelCurso;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+	public String getTelefoneResponsavel() {
+		return telefoneResponsavel;
+	}
+	public void setTelefoneResponsavel(String telefoneResponsavel) {
+		this.telefoneResponsavel = telefoneResponsavel;
+	}
+	public String getEmailResponsavel() {
+		return emailResponsavel;
+	}
+	public void setEmailResponsavel(String emailResponsavel) {
+		this.emailResponsavel = emailResponsavel;
+	}
+	public Cnpj getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(Cnpj cnpj) {
+		this.cnpj = cnpj;
+	}
+}
